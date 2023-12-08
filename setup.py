2f5baadf7b678d2b3dc39319b101ef86c3605257
Piotr Zdolski <dante383@protonmail.com>
2023-12-08 18:37:37 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
 	name='gkbus',
 	packages=find_packages(),
-	version='0.0.2',
+	version='0.0.3',
 	description='High-level KWP over K-line/CANbus library',
 	author='Dante383',
 	setup_requires=['pyftdi', 'scapy']
