4bebd475586114bce2c99a28597fff0b39168870
dmg210 <dmg@dg21network.com>
2024-11-17 12:55:25 +0000

    added long_description from README.md

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from setuptools import find_packages
 setup(
 	name='gkbus',
 	packages=find_packages(),
-	version='0.2.5',
+	version='0.2.7',
 	description='High-level KWP over K-line/CANbus library',
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
 	author='Dante383',
 	install_requires=['scapy==2.5.0', 'pyserial==3.5']
 )
