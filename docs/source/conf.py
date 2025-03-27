c2b661e52be8fca3fabb7512d2e2b1d738483053
Piotr Zdolski <dante383@protonmail.com>
2025-03-27 19:54:45 +0100

    readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,6 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
